f70d685f13c419307197e374730bb17cd7016a23
Rimantas <belauzas@yahoo.com>
2022-10-13 13:25:43 +0300

    App init

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Labas rytas, Lietuva!');
+const app = {};
+
+app.init = () => {
+    console.log('App init...');
+    // susikurti pirminius objektus:
+    // - folder
+    // - file
+    // gauti prisijungima prie DB
+    // paleisti pacia serverio logika
+    // reguliarus procesai:
+    // - atsinaujinti 3rd-party info (API, pvz valiutu kursai)
+    // - duomenu archivavimas
+    // - agregavimas (statistika)
+    // - pasalinti nebereikalingus objektus:
+    //   - failai (tokens)
+    //   - DB optimizavimas
+}
+
+app.init();
+
+export { app };
