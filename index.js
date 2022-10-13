99d1a9e10445f948551a7685baa332f3bb2ffd7d
Rimantas <belauzas@yahoo.com>
2022-10-13 14:18:16 +0300

    Server default response

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import { server } from "./lib/server.js";
+
 const app = {};
 
 app.init = () => {
-    console.log('App init...');
     // susikurti pirminius objektus:
     // - folder
     // - file
     // gauti prisijungima prie DB
     // paleisti pacia serverio logika
+    server.init();
+
     // reguliarus procesai:
     // - atsinaujinti 3rd-party info (API, pvz valiutu kursai)
     // - duomenu archivavimas
