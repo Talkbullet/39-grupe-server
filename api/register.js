75599cef8b000b0138fda6c5b7a9640be4d072d3
Rimantas <belauzas@yahoo.com>
2022-10-26 14:37:11 +0300

    Data buffer stringify trycatch

--- api/register.js
+++ api/register.js
@@ -1,25 +1,34 @@
 const handler = {};
 
 handler.init = (data, callback) => {
-    handler._handler[data.httpMethod]();
+    const allowedMethods = ['get', 'post', 'put', 'delete'];
+    if (allowedMethods.includes(data.httpMethod)) {
+        return handler._handler[data.httpMethod](data);
+    }
+    return 'ERROR: neleistinas http metodas';
 }
 
 handler._handler = {}
 
-handler._handler.get = () => {
+handler._handler.get = (data) => {
     console.log('register:GET logika...');
+    return 'GET response';
 }
 
-handler._handler.post = () => {
+handler._handler.post = (data) => {
     console.log('register:POST logika...');
+    console.log(data);
+    return 'POST response';
 }
 
-handler._handler.put = () => {
+handler._handler.put = (data) => {
     console.log('register:PUT logika...');
+    return 'PUT response';
 }
 
-handler._handler.delete = () => {
+handler._handler.delete = (data) => {
     console.log('register:DELETE logika...');
+    return 'DELETE response';
 }
 
 export default handler;
