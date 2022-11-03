efc2a3d29cd67dcd52d92ec7153318f22b978a51
Rimantas <belauzas@yahoo.com>
2022-11-03 14:40:55 +0200

    Cookies

--- api/login.js
+++ api/login.js
@@ -9,7 +9,7 @@ handler.init = async (data, callback) => {
     if (allowedMethods.includes(data.httpMethod)) {
         return await handler._handler[data.httpMethod](data);
     }
-    return 'ERROR: neleistinas http metodas';
+    return ['ERROR: neleistinas http metodas', 400];
 }
 
 handler._handler = {}
@@ -25,38 +25,54 @@ handler._handler.post = async (data) => {
 
     const [errEmail, msgEmail] = IsValid.email(email);
     if (errEmail) {
-        return msgEmail;
+        return [msgEmail, 400];
     }
 
     const [errPassword, msgPassword] = IsValid.password(password);
     if (errPassword) {
-        return msgPassword;
+        return [msgPassword, 400];
     }
 
     const salt = '5er8g4b526er5';
     const [hashErr, hashMsg] = utils.hash(password + salt);
     if (hashErr) {
-        return 'Nepavyko prijungti paskyros';
+        return ['Nepavyko prijungti paskyros', 500];
     }
 
     payload.password = hashMsg;
 
     const [readErr, readMsg] = await file.read('users', email + '.json');
     if (readErr) {
-        return 'Negeras email arba password';
+        return ['Negeras email arba password', 400];
     }
 
     const parseMsg = utils.parseJsonToObject(readMsg);
     if (Object.keys(parseMsg).length === 0) {
-        return 'Serverio klaida bandant atpazinti vartotoja';
+        return ['Serverio klaida bandant atpazinti vartotoja', 500];
     }
 
     if (parseMsg.password !== hashMsg) {
-        return 'Negeras email arba password';
+        return ['Negeras email arba password', 400];
     }
 
-    return ['OK', {
-        'Set-Cookie': `token=${utils.uuid(20)}`,
+    const tokenID = utils.uuid(20);
+
+    const [tokenErr] = await file.create('/tokens', tokenID + '.json', { email });
+    if (tokenErr) {
+        return ['Nepavyko prijungti vartotojo', 500];
+    }
+
+    const token = [
+        `token=${tokenID}`,
+        'Max-Age=3600',
+        'Path=/',
+        // 'Secure',
+        'HttpOnly',
+        'SameSite=Strict',
+    ];
+
+    return ['OK', 200, {
+        'Set-Cookie': `${token.join('; ')}`,
     }];
 
     // const [err, msg] = await file.create('/users', email + '.json', payload);
--- api/register.js
+++ api/register.js
@@ -9,7 +9,7 @@ handler.init = async (data, callback) => {
     if (allowedMethods.includes(data.httpMethod)) {
         return await handler._handler[data.httpMethod](data);
     }
-    return 'ERROR: neleistinas http metodas';
+    return ['ERROR: neleistinas http metodas', 400];
 }
 
 handler._handler = {}
@@ -27,33 +27,36 @@ handler._handler.post = async (data) => {
 
     const [errUsername, msgUsername] = IsValid.username(username);
     if (errUsername) {
-        return msgUsername;
+        return [msgUsername, 400];
     }
 
     const [errEmail, msgEmail] = IsValid.email(email);
     if (errEmail) {
-        return msgEmail;
+        return [msgEmail, 400];
     }
 
     const [errPassword, msgPassword] = IsValid.password(password);
     if (errPassword) {
-        return msgPassword;
+        return [msgPassword, 400];
     }
 
     if (password !== repeatPassword) {
-        return 'Slaptazodziai nesutampa';
+        return ['Slaptazodziai nesutampa', 400];
     }
 
     const salt = '5er8g4b526er5';
     const [hashErr, hashMsg] = utils.hash(password + salt);
     if (hashErr) {
-        return 'Nepavyko sukurti paskyros';
+        return ['Nepavyko sukurti paskyros', 500];
     }
 
     payload.password = hashMsg;
 
     const [err, msg] = await file.create('/users', email + '.json', payload);
-    return err ? 'Nepavyko sukurti paskyros' : msg;
+    if (err) {
+        return ['Nepavyko sukurti paskyros', 500];
+    }
+    return [msg];
 }
 
 handler._handler.put = (data) => {
